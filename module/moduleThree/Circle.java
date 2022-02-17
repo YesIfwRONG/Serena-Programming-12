2418846151f098f3bc10a7c775fa62763c6c5b1f
YesIfwRONG <92554649+YesIfwRONG@users.noreply.github.com>
2022-02-17 15:07:50 -0800

    lalala

---
+++ module/moduleThree/Circle.java
@@ -0,0 +1,21 @@
+package moduleThree;
+
+public class Circle extends TwoDShape {
+    public final double PI = Math.PI;
+    private double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return PI * Math.pow(radius,2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                getArea()+
+                '}';
+    }
+}
---
+++ module/moduleThree/Interface.java
@@ -0,0 +1,7 @@
+package moduleThree;
+
+public interface Interface {
+    void rotate90();
+    void rotate180();
+    void rotate(double degree);
+}
