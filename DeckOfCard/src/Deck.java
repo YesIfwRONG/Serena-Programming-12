741cb73e333b488a4c793d5debf61b20f436faca
YesIfwRONG <92554649+YesIfwRONG@users.noreply.github.com>
2022-03-07 17:31:24 -0800

    I finished!!!

--- DeckOfCard/src/Deck.java
+++ DeckOfCard/src/Deck.java
@@ -1,18 +1,64 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class Deck {
-    // set this to private later on;
-    public ArrayList<Card> deck;
+    private ArrayList<Card> deck;
+    private ArrayList<Card> hand;
     // construct a new deck
-    public void Deck(){
+    public Deck(){
         generateDeck();
-
     }
-    public static void shuffle(List deck){
+
+    public void shuffle(){
         Collections.shuffle(deck);
     }
+
+    public void sortDeck(){
+        ArrayList<Card> clubsTemp = new ArrayList<>();
+        ArrayList<Card> heartsTemp = new ArrayList<>();
+        ArrayList<Card> diamondsTemp = new ArrayList<>();
+        ArrayList<Card> spadesTemp = new ArrayList<>();
+        for (Card card : deck) {
+            if (card.getSuiteType() == Card.Suites.SPADES) {
+                spadesTemp.add(card);
+            } else if (card.getSuiteType() == Card.Suites.HEARTS) {
+                heartsTemp.add(card);
+            } else if (card.getSuiteType() == Card.Suites.CLUBS) {
+                clubsTemp.add(card);
+            } else {
+                diamondsTemp.add(card);
+            }
+        }
+        // sort the lists
+        clubsTemp =  sortByValue(clubsTemp);
+        heartsTemp = sortByValue(heartsTemp);
+        diamondsTemp = sortByValue(diamondsTemp);
+        spadesTemp = sortByValue(spadesTemp);
+        // create a new list to store the values
+        ArrayList<Card> tt = new ArrayList<>();
+        tt.addAll(spadesTemp);
+        tt.addAll(heartsTemp);
+        tt.addAll(clubsTemp);
+        tt.addAll(diamondsTemp);
+        // assign the value to tt
+        this.deck = tt;
+
+    }
+
+    public ArrayList<Card> sortByValue(ArrayList<Card>temp){
+        ArrayList<Card> ret = new ArrayList<>();
+        for (int i = 0; i < temp.size(); i++) {
+            for (Card card : temp) {
+                int cardValue = card.getCardValue();
+                if (cardValue == i + 2) {
+                    ret.add(card);
+                }
+            }
+        }
+        return ret;
+    }
+
+
     public void generateDeck(){
         ArrayList<Card> deck = new ArrayList<>(52);
         for (int i = 2; i < 15; i++) {
@@ -26,6 +72,28 @@ public class Deck {
         }
 
         this.deck = deck;
+    }
+
+    public void setHand(int size){
+        ArrayList<Card> hand = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            hand.add(deck.get(i));
+        }
+        this.hand = hand;
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void print(){
+        for(Card a:deck){
+            System.out.println(a);
+        }
     }
 
 }
--- DeckOfCard/src/Main.java
+++ DeckOfCard/src/Main.java
@@ -1,8 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        initiate();
-    }
-    public static void initiate(){
         Deck deck = new Deck();
+        userChoice(deck);
+    }
+
+    public static void test(){
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.print();
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+        deck.sortDeck();
+        deck.print();
+    }
+
+    public static void userChoice(Deck deck){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Do you need to shuffle the cards? (Y/N) ");
+        String ans = myObj.next();
+        if (ans.equals("Y")) {
+            deck.shuffle();
+            System.out.println("Shuffled. ");
+        } else if (ans.equals("N")) {
+            System.out.println("Received. ");
+        }else{
+            System.out.println("Answer not valid, we will keep everything as what it was. ");
+        }
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Enter a size of your cards: ");
+
+        int op = myObj.nextInt();
+        if (op > deck.getDeck().size() || op < 1) {
+            System.out.println("Sorry, size not valid. ");
+        } else {
+            deck.setHand(op);
+            System.out.println("This is your cards. ");
+            for (Card a : deck.getHand()) {
+                System.out.println(a);
+            }
+        }
+
     }
 }
