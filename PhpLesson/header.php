93cd0c2fd4054c59d519fb52cf58a991abb85fd7
Serena Chen <92554649+YesIfwRONG@users.noreply.github.com>
2022-06-21 11:47:12 -0700

    complete php lesson with all necessary functions, outlook needed

--- PhpLesson/header.php
+++ PhpLesson/header.php
@@ -1,11 +1,10 @@
-<?php
-include_once "includes/dbh.inc.php";
-?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Login System</title>
+    <link href="reset.css" rel="stylesheet" type="text/css">
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
@@ -16,11 +15,20 @@ include_once "includes/dbh.inc.php";
                     <li><a href="index.php">HOME</a></li>
                 </ul>
                 <div class="nav-login">
-                    <form>
+                    <?php
+                    if (isset($_SESSION['u_id'])) {
+                        echo'<form action="includes/logout.inc.php" method="POST">
+                        <button type="submit" name="submit">Log out</button>
+                        </form>';
+                    }else{
+                        echo'<form action="includes/login.inc.php" method="POST">
                         <input type="text" name="uid" placeholder="Username/email">
                         <input type="password" name="pwd" placeholder="Password">
                         <button type="submit" name="submit">Login</button>
-                    </form>
+                        </form>';
+                    }
+                    ?>
+
                     <a href="signup.php">Sign up</a>
                 </div>
             </div>
--- PhpLesson/index.php
+++ PhpLesson/index.php
@@ -4,6 +4,11 @@ include_once 'header.php';
 <section class="main-container">
     <div class="main-wrapper">
         <h2>Home Page</h2>
+        <?php
+        if (isset($_SESSION['u_id'])) {
+            echo $_SESSION['u_id'];
+        }
+        ?>
     </div>
 </section>
 <?php
